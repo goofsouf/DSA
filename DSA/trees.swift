84127979fc6008da5e6e6c05dd065622fc7c6805
Soufiane <els.soufiane@icloud.com>
2024-12-07 00:27:24 +0100

    added traversal algos

--- DSA/trees.swift
+++ DSA/trees.swift
@@ -23,3 +23,19 @@ func traversePreorder(root: node?, visit: (Int) -> Void = { print($0) }) {
     traversePreorder(root: root.left, visit: visit)
     traversePreorder(root: root.right, visit: visit)
 }
+
+func traverseInorder(root: node?, visit: (Int) -> Void = { print($0) }) {
+    guard let root = root else { return }
+
+    traverseInorder(root: root.left, visit: visit)
+    visit(root.value)
+    traverseInorder(root: root.right, visit: visit)
+}
+
+func traversePostorder(root: node?, visit: (Int) -> Void = { print($0)}){
+    guard let root = root else { return }
+    
+    traversePostorder(root: root.left, visit: visit)
+    traversePostorder(root: root.right, visit: visit)
+    visit(root.value)
+}
--- DSATesting/DSATesting.swift
+++ DSATesting/DSATesting.swift
@@ -312,7 +312,7 @@ struct DSATesting {
             radixSort(array: mediumArray) == mediumExpected,
             "radix not working")
     }
-    
+
     // MARK: Preorder Traverse
     @Test func testPreorderTraverse() async throws {
         let root = node(value: 1)
@@ -321,13 +321,47 @@ struct DSATesting {
         root.left?.left = node(value: 4)
         root.left?.right = node(value: 5)
 
-
         var result: [Int] = []
         let expected = [1, 2, 4, 5, 3]
         traversePreorder(root: root) { value in
             result.append(value)
         }
-        #expect( result == expected, "Preorder traverse not working")
+        #expect(result == expected, "Preorder traverse not working")
     }
 
+    // MARK: Inorder Traverse
+    @Test func testInorderTraverse() async throws {
+        let root = node(value: 4)
+        root.left = node(value: 2)
+        root.right = node(value: 6)
+        root.left?.left = node(value: 1)
+        root.left?.right = node(value: 3)
+        root.right?.left = node(value: 5)
+        root.right?.right = node(value: 7)
+
+        var result: [Int] = []
+        let expected = [1, 2, 3, 4, 5, 6, 7]
+        traverseInorder(root: root) { value in
+            result.append(value)
+        }
+        #expect(result == expected, "Inorder traverse not working")
+    }
+
+    // MARK: Postorder Traverse
+    @Test func testPostorder() async throws {
+        let root = node(value: 4)
+        root.left = node(value: 2)
+        root.right = node(value: 6)
+        root.left?.left = node(value: 1)
+        root.left?.right = node(value: 3)
+        root.right?.left = node(value: 5)
+        root.right?.right = node(value: 7)
+
+        var result: [Int] = []
+        let expected = [1, 3, 2, 5, 7, 6, 4]
+        traversePostorder(root: root) { value in
+            result.append(value)
+        }
+        #expect(result == expected, "ostorder traversal is not working")
+    }
 }
